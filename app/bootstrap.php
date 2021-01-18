9fbceffe357d257fecad7416213c12d954815431
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-01-18 13:33:18 +0200

    Lisatud raamistiku failid

---
+++ app/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+//const file
+require_once 'config/constants.php';
+
+// load libs
+require_once 'libs/Core.php';
+require_once 'libs/Controller.php';
+require_once 'libs/Database.php';
---
+++ app/config/constants.php
@@ -0,0 +1,13 @@
+<?php
+define('PROJECT_NAME', 'projectname');
+//app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// url root
+define('URLROOT','http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+// parameetrid db
+define('DB_HOST', 'host');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'dbname');
