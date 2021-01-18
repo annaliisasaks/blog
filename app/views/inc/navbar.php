0a10a79afa9f5b37bfdbe3dd329d5faccad41cd4
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-01-18 14:06:10 +0200

    Lisatud rakenduse nav p6hipaneel

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mg-3">
+    <div class="container">
+        <a href="<?php echo URLROOT?>" class="navbar-brand"><?php echo PROJECT_NAME?></a>
+        <button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-items"><a href="<?php echo URLROOT?>" class="nav-link">Home</a></li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
